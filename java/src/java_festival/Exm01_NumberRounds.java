ffd740a04d0e4ea1378f91e8b75ad78ce7db1ca4
songseungho <songseungho@variuimcbookpro>
2022-08-25 23:36:06 +0900

    첫번째 커

---
+++ java/src/java_festival/Exm01_NumberRounds.java
@@ -0,0 +1,22 @@
+package java_festival;
+
+import java.util.Scanner;
+
+public class Exm01_NumberRounds {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자 입력 : ");
+		int indata = sc.nextInt();
+		
+		if(indata%10>4)
+			indata =((indata/10)*10)+10;
+		else
+			indata = (indata/10)*10;
+
+			System.out.println("반올림 수 : "+ indata);
+	}
+
+}
