4bb75800abddc97a6047eafcd16f2ed714bb2bc9
songseungho <songseungho@localhost>
2022-09-05 23:07:38 +0900

    dao test를 위한 수정

--- Miniproject/src/model/DAO.java
+++ Miniproject/src/model/DAO.java
@@ -12,7 +12,7 @@ public class DAO {
 	ResultSet rs = null;
 	
 	
-	//insert회원가
+	//insert회원가입 
 	public int insert(DTO dto) {
 		int cnt =0;
 		
@@ -40,8 +40,24 @@ public class DAO {
 			 
 			 
 			 cnt = psmt.executeUpdate();
-			
-			
+			 if(cnt>0) {
+				 System.out.println("쿼리전송성공.");
+			 }else {
+				 System.out.println("쿼리전송 실패.");
+			 }
+			 
+			 psmt.close(); //끊어주기.
+			 cnt =0;//cnt 다시 초기화.
+			 
+			 
+			 String sql2 ="insert into log valuse(?,?,?)";
+			 psmt =conn.prepareStatement(sql2);
+			 psmt.setString(1, or_id);
+			 psmt.setString(2, start_day); //setDate가 있는데 잠시 대기.
+			 psmt.setString(3, last_day);
+			 cnt = psmt.executeUpdate();
+			 
+			 
 			
 		} catch (SQLException e) {
 			System.out.println("쿼리전송 실패.");
